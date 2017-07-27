b7d4d97987af65b446199dbdb214c9f62c94bc9b
CGI\jacky.siu <jacky.siu@cgi.com>
2017-07-27 18:59:09 -0400

    Added bubble fixed margin

--- react-app/src/components/indexGridList.js
+++ react-app/src/components/indexGridList.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {GridList, GridTile} from 'material-ui/GridList';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router'
+import Badge from 'material-ui/Badge';
+
 
 const styles = {
   root: {
@@ -25,18 +27,21 @@ const tilesData = [
     title: 'New Accounts',
     subtitle: 'New Accounts from clients',
     url: 'new-accounts',
+    bubble: '9',
   },
   {
     img: require('../images/acc-maint.jpg'),
     title: 'Account Maintenance',
     subtitle: 'Account Maintenance from clients',
     url: '#',
+    bubble: '0',
   },
   {
     img: require('../images/system-trade.jpg'),
     title: 'Pending Trades',
     subtitle: 'Handles pending trades',
     url: 'pending-trades',
+    bubble: '5',
   },
 ];
   
@@ -60,8 +65,15 @@ const IndexGridList = () => (
               key={tile.img}
               title={tile.title}
               subtitle={<span style={styles.subtitle}>{tile.subtitle}</span>}
-            >
-              <img alt="splash" src={tile.img} />
+            > 
+              <Badge
+                  badgeContent={tile.bubble}
+                  className="bubble"
+                  badgeStyle={{width: '100px', height:'100px', backgroundColor:'rgb(225, 24, 54)', color:'#fff', fontWeight: 600, fontSize: 75,}}
+                  style={{padding: '0px'}}
+              >
+              </Badge>
+              <img className="splash" alt="splash" src={tile.img} />
             </GridTile>
           </Link>
         ))}
--- react-app/src/pages/dashboard.js
+++ react-app/src/pages/dashboard.js
@@ -7,11 +7,6 @@ class Dashboard extends React.Component {
    render() {
       return (
         <div>
-            <h4 style= {{
-                    marginLeft: '20px',
-                }}>
-                <b>Dashboard</b>
-            </h4>
             <IndexGridList />
         </div>
       )
